e75cd0b63d7df091df97a09ec0fb822bdff847ca
ZabihullahNooriWardak <115200857+ZabihullahNooriWardak@users.noreply.github.com>
2023-10-10 17:32:02 +0430

    person class created

---
+++ person.rb
@@ -0,0 +1,18 @@
+
+class Person 
+   attr_accessor :id, :name, :age
+   def initialize(age, name = "unknown", parent_permission = true)
+      @name = name
+      @age = age
+   end
+
+   def private_of_age?
+    return  @age >= 18
+   end
+
+   def public_can_use_services?
+   return   private_of_age? || @parent_permission
+   end 
+end 
+
+
