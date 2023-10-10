22971d3b0969a7c3fa5ef86a2fa7770bedf60f72
ZabihullahNooriWardak <115200857+ZabihullahNooriWardak@users.noreply.github.com>
2023-10-10 18:31:42 +0430

    solved linter

--- person.rb
+++ person.rb
@@ -1,18 +1,16 @@
+class Person
+  attr_accessor :id, :name, :age
 
-class Person 
-   attr_accessor :id, :name, :age
-   def initialize(age, name = "unknown", parent_permission = true)
-      @name = name
-      @age = age
-   end
+  def initialize(age, name = "unknown", parent_permission = true)
+    @name = name
+    @age = age
+  end
 
-   def private_of_age?
-    return  @age >= 18
-   end
+  def private_of_age?
+    return @age >= 18
+  end
 
-   def public_can_use_services?
-   return   private_of_age? || @parent_permission
-   end 
-end 
-
-
+  def public_can_use_services?
+    return private_of_age? || @parent_permission
+  end
+end
