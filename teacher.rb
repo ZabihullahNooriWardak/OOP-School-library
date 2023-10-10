abe66990d0ba161e30160eb1d84e5b129b0f2e77
ZabihullahNooriWardak <115200857+ZabihullahNooriWardak@users.noreply.github.com>
2023-10-10 18:05:16 +0430

    teacher class created

---
+++ teacher.rb
@@ -0,0 +1,13 @@
+class Teacher < Person 
+ 
+    attr_accessor :specialization
+    def initialize(age,name,parent_permission,specialization)
+        super(age,name,parent_permission)
+        @specialization = specialization
+    end
+ 
+    def can_use_services?
+        return true
+    end
+
+end
