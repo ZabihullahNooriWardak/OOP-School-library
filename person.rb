4da29a70a1f7426c322060c231f31b38dbc1f031
ZabihullahNooriWardak <115200857+ZabihullahNooriWardak@users.noreply.github.com>
2023-10-10 18:56:06 +0430

    linter

--- person.rb
+++ person.rb
@@ -7,10 +7,10 @@ class Person
   end
 
   def private_of_age?
-    return @age >= 18
+    @age >= 18
   end
 
   def public_can_use_services?
-    return private_of_age? || @parent_permission
+    private_of_age? || @parent_permission
   end
 end
