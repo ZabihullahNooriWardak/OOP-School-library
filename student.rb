01f8147250104735b28864d2c5b01e35c026ebe5
ZabihullahNooriWardak <115200857+ZabihullahNooriWardak@users.noreply.github.com>
2023-10-10 19:21:50 +0430

    linter

--- student.rb
+++ student.rb
@@ -1,13 +1,12 @@
 class Student < Person
-    attr_accessor :classroom
-  
-    def initialize(age, name, parent_permission, classroom)
-      super(age, name, parent_permission)
-      @classroom = classroom
-    end
-  
-    def play_hooky
-      '¯(ツ)/¯' # Use single-quoted string, removed unnecessary escape, and trailing whitespace
-    end
+  attr_accessor :classroom
+
+  def initialize(age, name, parent_permission, classroom)
+    super(age, name, parent_permission)
+    @classroom = classroom
   end
-  
+
+  def play_hooky
+    '¯(ツ)/¯' # Use single-quoted string, removed unnecessary escape, and trailing whitespace
+  end
+end
