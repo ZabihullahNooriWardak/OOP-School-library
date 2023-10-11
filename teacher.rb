7a608270170c00adf450cc142efa062826abad17
ZabihullahNooriWardak <115200857+ZabihullahNooriWardak@users.noreply.github.com>
2023-10-11 16:54:21 +0430

    all the project requirement added

--- teacher.rb
+++ teacher.rb
@@ -1,12 +1,12 @@
-class Teacher < Person
-  attr_accessor :specialization
+  class Teacher < Person
+    attr_accessor :specialization
 
-  def initialize(age, name, parent_permission, specialization)
-    super(age, name, parent_permission)
-    @specialization = specialization
-  end
+    def initialize(age, name, parent_permission, specialization)
+      super(age, name, parent_permission)
+      @specialization = specialization
+    end
 
-  def can_use_services?
-    true
+    def can_use_services?
+      true
+    end
   end
-end
