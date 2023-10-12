d50644b3eadc7459ca75595d2a4991f8e68634c5
ZabihullahNooriWardak <115200857+ZabihullahNooriWardak@users.noreply.github.com>
2023-10-12 15:36:09 +0430

    rental class created

--- student.rb
+++ student.rb
@@ -4,6 +4,7 @@ class Student < Person
   def initialize(age, name, parent_permission, classroom)
     super(age, name, parent_permission)
     @classroom = classroom
+    classroom.add_student(self) 
   end
 
   def play_hooky
