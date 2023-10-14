c6aac448654d522d4a8cea6a50d368a7071b2b24
ZabihullahNooriWardak <115200857+ZabihullahNooriWardak@users.noreply.github.com>
2023-10-14 06:55:21 +0430

    linter fixed

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,13 +1,13 @@
 class Book
-    attr_accessor :title, :author, :rentals
-  
-    def initialize(title, author)
-      @title = title
-      @author = author
-      @rentals = []
-    end
-  
-    def add_rental(rental)
-      @rentals.push(rental)
-    end
+  attr_accessor :title, :author, :rentals
+
+  def initialize(title, author)
+    @title = title
+    @author = author
+    @rentals = []
   end
+
+  def add_rental(rental)
+    @rentals.push(rental)
+  end
+end
